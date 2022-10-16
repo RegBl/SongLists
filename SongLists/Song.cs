be5ff9e66eb241d102019dcfc25fafea23411811
RegBl <bnash1@madisoncollege.edu>
2022-10-15 22:24:11 -0500

    Fixed Index.razor, update Song.cs and SongList.cs models

--- SongLists/Song.cs
+++ SongLists/Song.cs
@@ -6,4 +6,5 @@ public class Song
     public string? Key { get; set; }
     public string? BPM { get; set; }
     public string? Lyrics { get; set; }
+    public string? AdditionalInfo { get; set; }
 }
--- SongLists/SongList.cs
+++ SongLists/SongList.cs
@@ -3,4 +3,7 @@
 public class SongList
 {
     public List<Song> Songs { get; set; }
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime DateTimeCreated { get; set; }
 }
